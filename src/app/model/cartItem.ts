614ee1ee616f26064563e78373d6ad8b933bfe0e
Rogelio Lotho <lothor@hotmail.com>
2023-03-30 16:02:24 -0600

    Delete src directory

--- src/app/model/cartItem.ts
+++
@@ -1,8 +0,0 @@
-import { Cuisines } from "./cuisines";
-
-export class CartItem {
-    constructor(public food: Cuisines) { }
-    quantity: number = 1;
-    price: number = this.food.price;
-    image: string = this.food.image;
-}
